785dadd204fdb6f85dd945c85ea00139ff1d8894
Siddharth Yadav <siddharth16268@iiitd.ac.in>
2019-02-09 21:42:55 +0530

    notebook checkpoint-with senti140

--- data/download.sh
+++ data/download.sh
@@ -1,1 +1,4 @@
 wget https://ndownloader.figshare.com/files/11929472 -O "Twitter_IIIT-H_Multilingual_Emoji.zip"
+wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
+tar -xvf aclImdb_v1.tar.gz
+wget http://4530.hostserv.eu/resources/corpus_v1.0.tsv -O  "german_corpus.tsv"
